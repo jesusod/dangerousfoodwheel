d9a8929e52b52d3716859e58c9bb95f73e3eb084
jesusod <jesusoterodevesa@gmail.com>
2022-02-23 14:14:25 +0100

    Last update logo

---
+++ librerias.js
@@ -0,0 +1,12 @@
+var nombres = new Array("Ana","Luis","Elena","Pedro", "Antonio", "Vanessa", "Jaime", "Sofía", "Alberto", "Carmen", "José","Olivia","Oliver");
+
+function generarNombres() {
+	var lista = document.getElementById("lista");  // Pilla objeto con id
+	lista.innerHTML = "";
+	
+	for (i =0; i<12; i++) {
+		var aleatorio = (Math.random()*12).toFixed(0);
+	
+		lista.innerHTML += nombres[aleatorio] + "<br>";
+	}
+}
